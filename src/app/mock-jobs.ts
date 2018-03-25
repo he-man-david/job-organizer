d4f250f9e9b430987b17bc5ec8f0ae20a6d5aa5f
davidhe184 <david.he184@gmail.com>
2018-03-25 17:31:25 -0400

    created mock jobs

---
+++ src/app/mock-jobs.ts
@@ -0,0 +1,16 @@
+import { Job } from "./job"
+
+export const Jobs: Job[] = [
+  { id: 1, name: "Apple", position: "Software Engineer II",
+    progress: 10, applied: true, status: "Application Submitted" },
+  { id: 2, name: "Google", position: "Senior Developer",
+    progress: 10, applied: true, status: "Application Submitted" },
+  { id: 3, name: "Samsung", position: "Java Developer II",
+    progress: 25, applied: true, status: "Phone Screen Completed!" },
+  { id: 4, name: "Baidu", position: "Data Developer II",
+    progress: 45, applied: true, status: "1st Round Interview Done!" },
+  { id: 5, name: "Facebook", position: "Software Engineer II",
+    progress: 85, applied: true, status: "3rd Round Interview Done!" },
+  { id: 6, name: "Tesla", position: "Full Stack Dev III",
+    progress: 100, applied: true, status: "Recieved Offer!" }
+]
