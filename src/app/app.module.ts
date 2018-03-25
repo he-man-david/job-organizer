2dbff0546fe3caf848bfeeb8a1c1a47b1659b8a7
davidhe184 <david.he184@gmail.com>
2018-03-25 14:40:41 -0400

    created job-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { JobListComponent } from './job-list/job-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule
