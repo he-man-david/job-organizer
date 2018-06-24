3e09747b6265dee38a840ed97030816084e2472a
davidhe184 <david.he184@gmail.com>
2018-06-24 17:16:48 -0500

    added progress bar styling

--- src/app/mock-jobs.ts
+++ src/app/mock-jobs.ts
@@ -2,15 +2,15 @@ import { Job } from "./job"
 
 export const Jobs: Job[] = [
   { id: 1, name: "Apple", position: "Software Engineer II",
-    progress: 10, applied: true, status: "Application Submitted" },
+    progress: 100, applied: true, status: "Application Submitted" },
   { id: 2, name: "Google", position: "Senior Developer",
-    progress: 10, applied: true, status: "Application Submitted" },
+    progress: 90, applied: true, status: "Application Submitted" },
   { id: 3, name: "Samsung", position: "Java Developer II",
-    progress: 25, applied: true, status: "Phone Screen Completed!" },
+    progress: 75, applied: true, status: "Phone Screen Completed!" },
   { id: 4, name: "Baidu", position: "Data Developer II",
-    progress: 45, applied: true, status: "1st Round Interview Done!" },
+    progress: 25, applied: true, status: "1st Round Interview Done!" },
   { id: 5, name: "Facebook", position: "Software Engineer II",
-    progress: 85, applied: true, status: "3rd Round Interview Done!" },
+    progress: 15, applied: true, status: "3rd Round Interview Done!" },
   { id: 6, name: "Tesla", position: "Full Stack Dev III",
-    progress: 100, applied: true, status: "Recieved Offer!" }
+    progress: 15, applied: true, status: "Recieved Offer!" }
 ]
